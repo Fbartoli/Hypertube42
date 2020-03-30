f468e41ecd3cc9438c83662ddab5ec4052cfe6c8
Fbartoli <fbartoli@monaco.edu>
2020-03-30 17:45:08 +0200

    list of movies

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    <h4>{{ title }}</h4>
+    <h4 class="title">{{ event.title }}</h4>
+    <h1>{{ event.description }}</h1>
+    <v-list>
+      <v-subheader>Cast</v-subheader>
+      <v-list-item-group v-model="cast" color="primary">
+        <v-list-item v-for="(actor, i) in event.cast" :key="i">
+          <v-list-item-content>
+            <v-list-item-title v-text="actor.name"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
   </div>
 </template>
 
@@ -8,14 +19,15 @@
 export default {
   data() {
     return {
-      title: "Movie title"
+      event: {
+        id: 1,
+        title: "Tomorrow never dies",
+        description: "James Bond kicking some asses",
+        cast: [{ id: 1, name: "Pierce Brosnan" }]
+      }
     };
   }
 };
 </script>
 
-<style lang="scss" scoped>
-h4 {
-  color: green;
-}
-</style>
+<style lang="scss" scoped></style>

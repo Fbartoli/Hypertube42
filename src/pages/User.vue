f6d12a4ad557f5f0f372fac370ac9a86f9ab7ccf
Matthieu Saubin <matt.saubin@gmail.com>
2020-05-11 22:16:36 +0200

    css changes on settings page

--- src/pages/User.vue
+++ src/pages/User.vue
@@ -1,158 +1,163 @@
 <template>
-  <v-card>
-    <v-card-title primary-title>
-      <div>
+  <div>
+    <v-card>
+      <v-card-title primary-title>
         <div class="headline">{{ $t('title') }}</div>
-      </div>
-    </v-card-title>
-    <div>
-      <div>
+      </v-card-title>
+
+      <br />
+      <v-divider></v-divider>
+      <br />
+
+      <v-container>
         <v-form ref="PersonalInfoForm" v-model="valid" lazy-validation>
-          <v-container>
-            <v-row>
-              <v-col cols="5">
-                <v-text-field
-                  v-model.trim="userData.username"
-                  :rules="usernameRules"
-                  :label="$t('username')"
-                  counter="20"
-                  required
-                />
-              </v-col>
-              <v-col cols="5"> </v-col>
-            </v-row>
-
-            <v-row>
-              <v-col cols="5">
-                <v-text-field
-                  v-model.trim="userData.firstname"
-                  :rules="nameRules"
-                  :label="$t('firstname')"
-                  counter="20"
-                  required
-                />
-              </v-col>
-              <v-col cols="5">
-                <v-text-field
-                  v-model.trim="userData.lastname"
-                  :rules="nameRules"
-                  :label="$t('lastname')"
-                  counter="20"
-                  required
-                />
-              </v-col>
-            </v-row>
-
-            <v-row>
-              <v-col cols="5">
-                <v-select
-                  v-model="userData.language"
-                  :items="languageList"
-                  :label="$t('language')"
-                  required
-                />
-              </v-col>
-            </v-row>
-
-            <v-row>
-              <v-col>
-                <v-btn
-                  @click="validatePersonalInfo"
-                  :disabled="!valid"
-                  color="blue lighten-4"
-                  class="mr-4"
-                >
-                  Update my Personal Information
-                </v-btn>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-form>
-        <v-form ref="EmailForm" v-model="valid" lazy-validation>
-          <v-container>
-            <v-row>
-              <v-col cols="10">
-                <v-text-field
-                  v-model.trim="userData.email"
-                  :rules="emailRules"
-                  :label="$t('email')"
-                  counter="42"
-                  required
-                />
-              </v-col>
-            </v-row>
-
-            <v-row>
-              <v-col>
-                <v-btn
-                  @click="validateEmail"
-                  :disabled="!valid"
-                  color="blue lighten-4"
-                  class="mr-4"
-                >
-                  Update my Email
-                </v-btn>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-form>
-        <v-container>
           <v-row>
             <v-col cols="5">
-              <v-card
-                :hover="true"
-                v-ripple="{ class: `blue--text` }"
-                color="deep-blue lighten-5"
-              >
-                <v-img
-                  aspect-ratio="2"
-                  class="spacer blue lighten-2"
-                  no-gutters
-                >
-                  <v-row align="center" class="fill-height">
-                    <v-col align="center" class="pa-0" cols="12">
-                      <br />
-                      <v-avatar class="profile indigo accent-4" size="164">
-                        <v-img
-                          v-if="showPictures[0]"
-                          :src="`data:image/*;base64,${showPictures[0]}`"
-                        />
-                        <v-img
-                          v-else
-                          :src="`data:image/*;base64,${userData.avatar}`"
-                        />
-                      </v-avatar>
-                      <v-card-subtitle>
-                        <div>
-                          <div
-                            class="headline font-weight-bold blue--text text--accent-4"
-                          >
-                            {{ userData.username }}
-                          </div>
-                          <a
-                            class="title font-italic blue--text text--accent-3"
-                          >
-                            {{ userData.firstname }}
-                          </a>
-                          <a
-                            class="title font-italic blue--text text--accent-3"
-                          >
-                            {{ userData.lastname }}
-                          </a>
-                        </div>
-                      </v-card-subtitle>
-                    </v-col>
-                  </v-row>
-                </v-img>
-              </v-card>
+              <v-text-field
+                v-model.trim="userData.username"
+                :rules="usernameRules"
+                :label="$t('username')"
+                counter="20"
+                required
+              />
+            </v-col>
+            <v-col cols="5"> </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col cols="5">
+              <v-text-field
+                v-model.trim="userData.firstname"
+                :rules="nameRules"
+                :label="$t('firstname')"
+                counter="20"
+                required
+              />
+            </v-col>
+            <v-col cols="5">
+              <v-text-field
+                v-model.trim="userData.lastname"
+                :rules="nameRules"
+                :label="$t('lastname')"
+                counter="20"
+                required
+              />
             </v-col>
           </v-row>
-        </v-container>
-        <v-form id="myForm" ref="AvatarForm" v-model="valid" lazy-validation>
-          <v-container>
-            <br /><br />
-            <v-row>
-              <v-col cols="10">
+
+          <v-row>
+            <v-col cols="5">
+              <v-select
+                v-model="userData.language"
+                :items="languageList"
+                :label="$t('language')"
+                required
+              />
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col>
+              <v-btn
+                @click="validatePersonalInfo"
+                :disabled="!valid"
+                color="blue lighten-4"
+                class="mr-4"
+              >
+                Update my Personal Information
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-form>
+      </v-container>
+
+      <br />
+      <v-divider></v-divider>
+      <br />
+
+      <v-container>
+        <v-form ref="EmailForm" v-model="valid" lazy-validation>
+          <v-row>
+            <v-col cols="10">
+              <v-text-field
+                v-model.trim="userData.email"
+                :rules="emailRules"
+                :label="$t('email')"
+                counter="42"
+                required
+              />
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col>
+              <v-btn
+                @click="validateEmail"
+                :disabled="!valid"
+                color="blue lighten-4"
+                class="mr-4"
+              >
+                Update my Email
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-form>
+      </v-container>
+
+      <br />
+      <v-divider></v-divider>
+      <br />
+
+      <v-container>
+        <v-row align="center">
+          <v-col cols="5">
+            <v-card
+              :hover="true"
+              v-ripple="{ class: `blue--text` }"
+              color="deep-blue lighten-5"
+            >
+              <v-img aspect-ratio="2" class="spacer blue lighten-2" no-gutters>
+                <v-row align="center" class="fill-height">
+                  <v-col align="center" class="pa-0" cols="12">
+                    <br />
+                    <v-avatar class="profile indigo accent-4" size="164">
+                      <v-img
+                        v-if="showPictures[0]"
+                        :src="`data:image/*;base64,${showPictures[0]}`"
+                      />
+                      <v-img
+                        v-else
+                        :src="`data:image/*;base64,${userData.avatar}`"
+                      />
+                    </v-avatar>
+                    <v-card-subtitle>
+                      <div>
+                        <div
+                          class="headline font-weight-bold blue--text text--accent-4"
+                        >
+                          {{ userData.username }}
+                        </div>
+                        <a class="title font-italic blue--text text--accent-3">
+                          {{ userData.firstname }}
+                        </a>
+                        <a class="title font-italic blue--text text--accent-3">
+                          {{ userData.lastname }}
+                        </a>
+                      </div>
+                    </v-card-subtitle>
+                  </v-col>
+                </v-row>
+              </v-img>
+            </v-card>
+          </v-col>
+          <v-col cols="5">
+            <v-row justify="center">
+              <v-form
+                id="myForm"
+                ref="AvatarForm"
+                v-model="valid"
+                lazy-validation
+              >
                 <v-file-input
                   @change="displayImage(uploadPic.mypic)"
                   :rules="mypicsRules"
@@ -165,10 +170,7 @@
                   label="Profile Picture"
                   truncate-length="42"
                 />
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col>
+                <br />
                 <v-btn
                   @click="validateAvatar"
                   :disabled="!valid"
@@ -177,27 +179,29 @@
                 >
                   Update my Avatar
                 </v-btn>
-              </v-col>
+              </v-form>
             </v-row>
-          </v-container>
-        </v-form>
-        <v-container>
-          <v-row class="font-italic">
-            <v-col>
-              <br />
-              <router-link :to="{ name: 'resetpasswordonline' }">
-                {{ $t('Reset password') }}<br /><br />
-              </router-link>
-            </v-col>
-          </v-row>
-          <br />
-          <v-btn @click="getUser()" color="blue lighten-4" class="mr-4">
-            GetUser_TEST
-          </v-btn>
-        </v-container>
-      </div>
-    </div>
-  </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+      <br />
+      <v-divider></v-divider>
+      <v-container>
+        <v-row class="font-italic">
+          <v-col>
+            <br />
+            <router-link :to="{ name: 'resetpasswordonline' }">
+              {{ $t('Reset password') }}<br /><br />
+            </router-link>
+          </v-col>
+        </v-row>
+        <br />
+        <v-btn @click="getUser()" color="blue lighten-4" class="mr-4">
+          GetUser_TEST
+        </v-btn>
+      </v-container>
+    </v-card>
+  </div>
 </template>
 
 <script>
@@ -249,12 +253,6 @@ export default {
       ],
     }
   },
-  // props: {
-  //   userInfo: {
-  //     type: Object,
-  //     required: true,
-  //   },
-  // },
   computed: {
     ...mapGetters({
       userData: 'App/storeUser',
@@ -278,30 +276,6 @@ export default {
       this.updateEmail({ payloadPutEmail })
       // }
     },
-    // validateEmail2() {
-    //   const options = {
-    //     method: 'put',
-    //     url: `https://hypertube42.herokuapp.com/users/user/${this.currentUsername}/email`,
-    //     data: {
-    //       email: this.userData.email,
-    //     },
-    //     headers: {
-    //       'x-access-token': this.userData.token,
-    //     },
-    //   }
-    //   if (this.$refs.EmailForm.validate()) {
-    //     axios(options)
-    //       .then(response => {
-    //         console.log('email updated! ', response)
-    //         //   // this.$store.dispatch('interact/setMessage', 'Personal information updated !')
-    //         //   this.$router.push('/')
-    //       })
-    //       // eslint-disable-next-line
-    //       .catch(error => {})
-    //     // this.$router.push('/settings')
-    //   }
-    // },
-
     validatePersonalInfo() {
       const payloadPutUser = {
         // THIS ?
@@ -356,14 +330,16 @@ export default {
     // },
 
     validateAvatar() {
+      // console.log('ROUTER', router)
       if (this.$refs.AvatarForm.validate()) {
         const data = new FormData()
+        const xhr = new XMLHttpRequest()
+        const self = this
+
         if (this.uploadPic.mypic) {
           data.append('avatar', this.uploadPic.mypic)
         }
-        const xhr = new XMLHttpRequest()
-        // xhr.withCredentials = true
-        // const self = this
+
         // Returns an unsigned short, the state of the request.
         xhr.addEventListener('readystatechange', function() {
           // 4 means the request is DONE, operation completed
@@ -371,6 +347,16 @@ export default {
             if (this.status === 200 || this.status === '200') {
               // self.$store.dispatch('storeAction', 'Pictures updated !')
               this.showPictures = null
+              const notification = {
+                type: 200,
+                message: 'Profile picture updated !',
+              }
+              self.$store.dispatch('Notifications/add', notification, {
+                root: true,
+              })
+              self.$router.push({
+                name: 'home',
+              })
             }
           }
         })
@@ -381,7 +367,6 @@ export default {
         xhr.setRequestHeader('Accept', '*/*')
         xhr.setRequestHeader('Cache-Control', 'no-cache')
         xhr.setRequestHeader('Access-Control-Allow-Origin', true)
-        // xhr.setRequestHeader('Access-Control-Allow-Credentials', true)
         xhr.setRequestHeader('x-access-token', this.userData.token)
         // xhr.setRequestHeader('Accept-Encoding', 'gzip, deflate')
         // xhr.setRequestHeader('Connection', 'keep-alive')

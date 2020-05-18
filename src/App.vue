70dd853d9a0789c8dbf1a2fe75eccc32f1d8200c
Fbartoli <fbartoli@monaco.edu>
2020-05-18 10:26:21 +0200

    ui changes v-content

--- src/App.vue
+++ src/App.vue
@@ -198,8 +198,8 @@
     <!-- Sizes your content based upon application components -->
     <v-content>
       <!-- Provides the application the proper gutter -->
-      <v-container fluid>
-        <v-row align="center" justify="center">
+      <v-container fluid class="fill-height">
+        <v-row align="end" justify="center">
           <v-col cols="12">
             <router-view :key="$route.fullPath"></router-view>
           </v-col>
